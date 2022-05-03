a87d11b6fcd36c177172f725404e20f58a33e13f
Eric Hanson <ehanson@mit.edu>
2022-05-03 11:08:25 -0600

    rdi-77-update-TimdexRecord-class
    * Update TimdexRecord class to account for additional Datacite fields
    * Update unit tests and fixtures to account for the new fields
    * Remove unnecessary list around Note value
    * Update source_base_url value for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,17 @@ from click.testing import CliRunner
 
 from transmogrifier.helpers import parse_xml_records
 from transmogrifier.models import (
+    AlternateTitle,
     Contributor,
     Date,
     Date_Range,
+    Funder,
     Identifier,
+    Location,
     Note,
+    RelatedItem,
+    Rights,
+    Subject,
     TimdexRecord,
 )
 from transmogrifier.sources.datacite import Datacite
@@ -67,7 +73,7 @@ def datacite_record_partial():
         Datacite,
         source="cool-repo",
         source_name="A Cool Repository",
-        source_base_url="https://example.com/item123",
+        source_base_url="https://example.com/",
     )
 
 
@@ -75,7 +81,7 @@ def datacite_record_partial():
 def timdex_record_required_fields():
     return TimdexRecord(
         source="A Cool Repository",
-        source_link="https://example.com/item123",
+        source_link="https://example.com/",
         timdex_record_id="cool-repo:123",
         title="Some Data About Trees",
     )
@@ -85,9 +91,10 @@ def timdex_record_required_fields():
 def timdex_record_all_fields_and_subfields():
     return TimdexRecord(
         source="A Cool Repository",
-        source_link="https://example.com/item123",
+        source_link="https://example.com/",
         timdex_record_id="cool-repo:123",
         title="Some Data About Trees",
+        alternate_titles=[AlternateTitle(value="Alt title", kind="alternative")],
         content_type=["dataset"],
         contributors=[
             Contributor(
@@ -106,7 +113,29 @@ def timdex_record_all_fields_and_subfields():
                 range=Date_Range(gt="2019-01-01", lt="2019-06-30"),
             ),
         ],
+        edition="2nd revision",
+        file_formats=["application/pdf"],
+        format="electronic resource",
+        funding_information=[
+            Funder(
+                funder_name="Funding Foundation",
+                funder_identifier="4356",
+                funder_identifier_type="Crossref FunderID",
+                award_number="3124",
+                award_uri="http://awards.example/",
+            )
+        ],
         identifiers=[Identifier(value="123", kind="doi")],
-        notes=[Note(value=["This book is awesome"], kind="opinion")],
+        languages=["en_US"],
+        locations=[Location(value="Data gathered at a place")],
+        notes=[Note(value="This book is awesome", kind="opinion")],
         publication_information=["Version 1.0"],
+        related_items=[
+            RelatedItem(uri="http://doi.example/123", kind="isReferencedBy")
+        ],
+        rights=[
+            Rights(description="People may use this", uri="http://rights.example/"),
+        ],
+        subjects=[Subject(value="Stuff", kind="LCSH")],
+        summary=["This is data."],
     )
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,45 +1,92 @@
 from pytest import raises
 
-from transmogrifier.models import Contributor, Date, Date_Range, Identifier, Note
+from transmogrifier.models import (
+    AlternateTitle,
+    Contributor,
+    Date,
+    Date_Range,
+    Identifier,
+    Location,
+    Note,
+    RelatedItem,
+    Subject,
+)
 
 
 def test_timdex_record_required_fields_only(timdex_record_required_fields):
     assert timdex_record_required_fields.source == "A Cool Repository"
-    assert timdex_record_required_fields.source_link == "https://example.com/item123"
+    assert timdex_record_required_fields.source_link == "https://example.com/"
     assert timdex_record_required_fields.timdex_record_id == "cool-repo:123"
     assert timdex_record_required_fields.title == "Some Data About Trees"
+    assert timdex_record_required_fields.alternate_titles is None
     assert timdex_record_required_fields.content_type is None
     assert timdex_record_required_fields.contributors is None
     assert timdex_record_required_fields.dates is None
+    assert timdex_record_required_fields.edition is None
+    assert timdex_record_required_fields.file_formats is None
+    assert timdex_record_required_fields.format is None
+    assert timdex_record_required_fields.funding_information is None
     assert timdex_record_required_fields.identifiers is None
+    assert timdex_record_required_fields.languages is None
+    assert timdex_record_required_fields.locations is None
     assert timdex_record_required_fields.notes is None
     assert timdex_record_required_fields.publication_information is None
+    assert timdex_record_required_fields.related_items is None
+    assert timdex_record_required_fields.rights is None
+    assert timdex_record_required_fields.subjects is None
+    assert timdex_record_required_fields.summary is None
 
 
 def test_timdex_record_required_subfields_only(timdex_record_required_fields):
     timdex_record_required_fields.contributors = [Contributor(value="Smith, Jane")]
     timdex_record_required_fields.identifiers = [Identifier(value="123")]
-    timdex_record_required_fields.notes = [Note(value=["This book is awesome"])]
+    timdex_record_required_fields.notes = [Note(value="This book is awesome")]
+    timdex_record_required_fields.alternate_titles = [AlternateTitle(value="Alt Title")]
+    timdex_record_required_fields.locations = [
+        Location(value="Data gathered at a place")
+    ]
+    timdex_record_required_fields.related_items = [
+        RelatedItem(uri="http://doi.example/123")
+    ]
+    timdex_record_required_fields.subjects = [Subject(value="Stuff")]
     assert timdex_record_required_fields.source == "A Cool Repository"
-    assert timdex_record_required_fields.source_link == "https://example.com/item123"
+    assert timdex_record_required_fields.source_link == "https://example.com/"
     assert timdex_record_required_fields.timdex_record_id == "cool-repo:123"
     assert timdex_record_required_fields.title == "Some Data About Trees"
+    assert timdex_record_required_fields.alternate_titles[0].value == "Alt Title"
     assert timdex_record_required_fields.content_type is None
     assert timdex_record_required_fields.contributors[0].value == "Smith, Jane"
     assert timdex_record_required_fields.dates is None
+    assert timdex_record_required_fields.edition is None
+    assert timdex_record_required_fields.file_formats is None
+    assert timdex_record_required_fields.format is None
+    assert timdex_record_required_fields.funding_information is None
     assert timdex_record_required_fields.identifiers[0].value == "123"
-    assert timdex_record_required_fields.notes[0].value == ["This book is awesome"]
+    assert timdex_record_required_fields.languages is None
+    assert (
+        timdex_record_required_fields.locations[0].value == "Data gathered at a place"
+    )
+    assert timdex_record_required_fields.notes[0].value == "This book is awesome"
     assert timdex_record_required_fields.publication_information is None
+    assert (
+        timdex_record_required_fields.related_items[0].uri == "http://doi.example/123"
+    )
+    assert timdex_record_required_fields.rights is None
+    assert timdex_record_required_fields.subjects[0].value == "Stuff"
+    assert timdex_record_required_fields.summary is None
 
 
 def test_timdex_record_all_fields_and_subfields(timdex_record_all_fields_and_subfields):
     assert timdex_record_all_fields_and_subfields.source == "A Cool Repository"
-    assert (
-        timdex_record_all_fields_and_subfields.source_link
-        == "https://example.com/item123"
-    )
+    assert timdex_record_all_fields_and_subfields.source_link == "https://example.com/"
     assert timdex_record_all_fields_and_subfields.timdex_record_id == "cool-repo:123"
     assert timdex_record_all_fields_and_subfields.title == "Some Data About Trees"
+    assert (
+        timdex_record_all_fields_and_subfields.alternate_titles[0].value == "Alt title"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.alternate_titles[0].kind == "alternative"
+    )
     assert timdex_record_all_fields_and_subfields.content_type == ["dataset"]
     assert timdex_record_all_fields_and_subfields.contributors[0].value == "Smith, Jane"
     assert timdex_record_all_fields_and_subfields.contributors[0].affiliation == ["MIT"]
@@ -57,15 +104,63 @@ def test_timdex_record_all_fields_and_subfields(timdex_record_all_fields_and_sub
     )
     assert timdex_record_all_fields_and_subfields.dates[1].range.gt == "2019-01-01"
     assert timdex_record_all_fields_and_subfields.dates[1].range.lt == "2019-06-30"
+    assert timdex_record_all_fields_and_subfields.edition == "2nd revision"
+    assert timdex_record_all_fields_and_subfields.file_formats == ["application/pdf"]
+    assert timdex_record_all_fields_and_subfields.format == "electronic resource"
+    assert (
+        timdex_record_all_fields_and_subfields.funding_information[0].funder_name
+        == "Funding Foundation"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.funding_information[0].funder_identifier
+        == "4356"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.funding_information[
+            0
+        ].funder_identifier_type
+        == "Crossref FunderID"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.funding_information[0].award_number
+        == "3124"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.funding_information[0].award_uri
+        == "http://awards.example/"
+    )
     assert timdex_record_all_fields_and_subfields.identifiers[0].value == "123"
     assert timdex_record_all_fields_and_subfields.identifiers[0].kind == "doi"
-    assert timdex_record_all_fields_and_subfields.notes[0].value == [
-        "This book is awesome"
-    ]
+    assert timdex_record_all_fields_and_subfields.languages == ["en_US"]
+    assert (
+        timdex_record_all_fields_and_subfields.locations[0].value
+        == "Data gathered at a place"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.notes[0].value == "This book is awesome"
+    )
+
     assert timdex_record_all_fields_and_subfields.notes[0].kind == "opinion"
     assert timdex_record_all_fields_and_subfields.publication_information == [
         "Version 1.0"
     ]
+    assert (
+        timdex_record_all_fields_and_subfields.related_items[0].uri
+        == "http://doi.example/123"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.related_items[0].kind == "isReferencedBy"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.rights[0].description
+        == "People may use this"
+    )
+    assert (
+        timdex_record_all_fields_and_subfields.rights[0].uri == "http://rights.example/"
+    )
+    assert timdex_record_all_fields_and_subfields.subjects[0].value == "Stuff"
+    assert timdex_record_all_fields_and_subfields.subjects[0].kind == "LCSH"
+    assert timdex_record_all_fields_and_subfields.summary[0] == "This is data."
 
 
 def test_record_asdict_filters_empty_fields(
@@ -73,7 +168,7 @@ def test_record_asdict_filters_empty_fields(
 ):
     assert timdex_record_required_fields.asdict() == {
         "source": "A Cool Repository",
-        "source_link": "https://example.com/item123",
+        "source_link": "https://example.com/",
         "timdex_record_id": "cool-repo:123",
         "title": "Some Data About Trees",
     }
@@ -82,9 +177,10 @@ def test_record_asdict_filters_empty_fields(
 def test_record_asdict_includes_all_fields(timdex_record_all_fields_and_subfields):
     assert timdex_record_all_fields_and_subfields.asdict() == {
         "source": "A Cool Repository",
-        "source_link": "https://example.com/item123",
+        "source_link": "https://example.com/",
         "timdex_record_id": "cool-repo:123",
         "title": "Some Data About Trees",
+        "alternate_titles": [{"kind": "alternative", "value": "Alt title"}],
         "content_type": ["dataset"],
         "contributors": [
             {
@@ -103,9 +199,29 @@ def test_record_asdict_includes_all_fields(timdex_record_all_fields_and_subfield
                 "range": {"gt": "2019-01-01", "lt": "2019-06-30"},
             },
         ],
+        "edition": "2nd revision",
+        "file_formats": ["application/pdf"],
+        "format": "electronic resource",
+        "funding_information": [
+            {
+                "award_number": "3124",
+                "award_uri": "http://awards.example/",
+                "funder_identifier": "4356",
+                "funder_identifier_type": "Crossref FunderID",
+                "funder_name": "Funding Foundation",
+            }
+        ],
         "identifiers": [{"value": "123", "kind": "doi"}],
-        "notes": [{"value": ["This book is awesome"], "kind": "opinion"}],
+        "languages": ["en_US"],
+        "locations": [{"value": "Data gathered at a place"}],
+        "notes": [{"value": "This book is awesome", "kind": "opinion"}],
         "publication_information": ["Version 1.0"],
+        "related_items": [{"kind": "isReferencedBy", "uri": "http://doi.example/123"}],
+        "rights": [
+            {"description": "People may use this", "uri": "http://rights.example/"}
+        ],
+        "subjects": [{"kind": "LCSH", "value": "Stuff"}],
+        "summary": ["This is data."],
     }
 
 
